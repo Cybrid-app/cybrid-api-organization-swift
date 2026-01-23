d0e0b80c193ccd5585e0202ce79737abf3aba270
brent-cybrid <brent@cybrid.app>
2026-01-23 13:47:44 +0000

    v0.126.69

--- CybridApiOrganizationSwift.podspec
+++ CybridApiOrganizationSwift.podspec
@@ -4,7 +4,7 @@ Pod::Spec.new do |s|
   s.osx.deployment_target = '10.11'
   s.tvos.deployment_target = '9.0'
   s.watchos.deployment_target = '3.0'
-  s.version = '0.126.68'
+  s.version = '0.126.69'
   s.source = { :git => "https://github.com/Cybrid-app/cybrid-api-organization-swift.git", :tag => "v#{s.version}" }
   s.authors = 'Cybrid Technology Inc.'
   s.license = { :type => "Apache-2.0", :file => "LICENSE"}
@@ -12,5 +12,5 @@ Pod::Spec.new do |s|
   s.summary = 'Cybrid iOS Organization API'
   s.description = 'Cybrid Organization API for Swift'
   s.source_files = 'CybridApiOrganizationSwift/APIClient/**/*.swift'
-  s.dependency 'AnyCodable-FlightSchool', '~> 0.6.1'
+  # -------
 end
--- Package.swift
+++ Package.swift
@@ -18,15 +18,15 @@ let package = Package(
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.1"),
+        // ------
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CybridApiOrganizationSwift",
-            dependencies: ["AnyCodable", ],
+            // Add BigInt dependency
+            dependencies: [],
             path: "CybridApiOrganizationSwift/APIClient"
         ),
     ]
